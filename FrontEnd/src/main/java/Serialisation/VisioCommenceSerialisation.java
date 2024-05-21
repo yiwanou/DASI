2f1c88ef27531a1d425b8cb7060c1c2802d62b96
Qinshu Dai <qdai@insa-lyon.fr>
2024-05-21 11:46:55 +0200

    visio

--- FrontEnd/src/main/java/Serialisation/VisioCommenceSerialisation.java
+++ FrontEnd/src/main/java/Serialisation/VisioCommenceSerialisation.java
@@ -5,10 +5,40 @@
  */
 package Serialisation;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author qdai
  */
 public class VisioCommenceSerialisation {
-    
+
+    public void appliquer(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        PrintWriter out = null;
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+        JsonObject container = new JsonObject();
+
+
+        Boolean isStart = (Boolean)req.getAttribute("isStart");
+         container.addProperty("isStart", isStart);
+        
+        if (isStart) {
+            container.addProperty("duree", (Long) req.getAttribute("duree"));
+
+        }
+
+        res.setContentType("application/json;charset=UTF-8");
+        out = res.getWriter();
+        out.println(gson.toJson(container));
+        out.close();
+
+    }
 }
--- FrontEnd/src/main/java/Serialisation/VisioTermineSerialisation.java
+++ FrontEnd/src/main/java/Serialisation/VisioTermineSerialisation.java
@@ -5,10 +5,36 @@
  */
 package Serialisation;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import metier.Intervention;
+
 /**
  *
  * @author qdai
  */
 public class VisioTermineSerialisation {
-    
+
+    public void appliquer(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        PrintWriter out = null;
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+        JsonObject container = new JsonObject();
+
+        Boolean isEnd = (Boolean) req.getAttribute("isEnd");
+        container.addProperty("isEnd", isEnd);
+
+
+        res.setContentType("application/json;charset=UTF-8");
+        out = res.getWriter();
+        out.println(gson.toJson(container));
+        out.close();
+
+    }
 }
